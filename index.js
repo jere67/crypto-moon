e51bd367b09edc2269856f6fa3335ecf3739ce71
Jeremy Moon <jeremymoon567@gmail.com>
2024-07-11 23:48:44 +0900

    initial market section working

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
-const API_URL = "https://blogmoon.onrender.com";
+const API_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=${currentPage}&sparkline=false";
 
 app.use(express.static("public")); // Use for client file
 
@@ -12,68 +12,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/", async (req, res) => {
-  res.render("index.ejs");
-});
-
-app.get("/blog", async (req, res) => {
   try {
-    const response = await axios.get(`${API_URL}/posts`);
-    res.render("blog.ejs", { posts: response.data });
+    const response = await axios.get(`${API_URL}`, {
+      headers: {
+        x_cg_pro_api_key: process.env.token
+      }
+    });
+    console.log(response.data);
+    res.render("index.ejs", { data: response.data });
   } catch (error) {
     res.status(500).json({ message: "Error fetching posts" });
-  }
-});
-
-app.get("/create", (req, res) => {
-  res.render("create.ejs");
-});
-
-app.get("/edit/:id", async (req, res) => {
-  try {
-    const response = await axios.get(`${API_URL}/posts/${req.params.id}`);
-    console.log(response.data);
-    res.render("edit.ejs", {
-      heading: "Edit Post",
-      submit: "Update Post",
-      post: response.data,
-    });
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching post" });
-  }
-});
-
-// Create a new post
-app.post("/api/posts", async (req, res) => {
-  try {
-    const response = await axios.post(`${API_URL}/posts`, req.body);
-    console.log(response.data);
-    res.redirect("/blog");
-  } catch (error) {
-    res.status(500).json({ message: "Error creating post" });
-  }
-});
-
-// Partially update a post
-app.post("/api/posts/:id", async (req, res) => {
-  console.log("called");
-  try {
-    const response = await axios.patch(
-      `${API_URL}/posts/${req.params.id}`,
-      req.body
-    );
-    res.redirect("/blog");
-  } catch (error) {
-    res.status(500).json({ message: "Error updating post" });
-  }
-});
-
-// Delete a post
-app.get("/api/posts/delete/:id", async (req, res) => {
-  try {
-    await axios.delete(`${API_URL}/posts/${req.params.id}`);
-    res.redirect("/blog");
-  } catch (error) {
-    res.status(500).json({ message: "Error deleting post" });
   }
 });
 
